9e4ad1c4457f801b85d877652eb33008177b7bff
olab <ola@example.com>
2020-04-15 18:03:59 +0200

    project has been initialized

---
+++ src/app/places/offers/new-offer/new-offer.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+
+import { NewOfferPage } from './new-offer.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NewOfferPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    HttpClientModule
+  ],
+  declarations: [NewOfferPage]
+})
+export class NewOfferPageModule {}
